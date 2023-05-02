c0fcd14dea451f68ed8b2f13fc71883870f1e41d
Samuel Iansen <iansen2022@gmail.com>
2023-05-01 21:22:11 -0300

    Kotlin variável mutável e imutável

---
+++ main2.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    //Variavel mutável: Var Nome
+    val idade = 29
+    var nome = ("samuel iansen: " +idade)
+    println(nome);
+    val idade1 = 39
+    var nome1 = ("Carla Souza Santos: " +idade1)
+    println(nome1);
+    val idade2 = 41
+    var nome2 = ("Valentina Maria: " +idade2)
+    println(nome2);
+
+    //Variável imutável: Val idade
+}
